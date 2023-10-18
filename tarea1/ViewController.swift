c5d4c0e870420ab4b84bac610d624360aff5049f
Joan <joan_hernandez@cev.com>
2023-10-18 10:32:33 +0100

    Initial Commit

---
+++ tarea1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarea1
+//
+//  Created by user197292 on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
