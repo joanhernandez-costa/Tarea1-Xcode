ad628b1e94202fa70f7ea9316a64ed113c2a3bf2
Joan <joan_hernandez@cev.com>
2023-10-25 13:10:48 +0100

    Add transition from showCards to ChooseCards

--- tarea1/GameViewController.swift
+++ tarea1/GameViewController.swift
@@ -15,28 +15,39 @@ class GameViewController: UIViewController {
                             UIImage(named: "aguila")!,
                             UIImage(named: "tortuga")!]
     
-    
+    var indices: [Int] = []
     
     @IBOutlet weak var showCardsImageView: UIImageView!
+    let gameSettings: GameSettingsViewController = GameSettingsViewController()
     
+    var numberOfCards: Int = 0
+    var cardTime: Float = 0.0
+    
+    var timer: Timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let gameSettings: GameSettingsViewController = GameSettingsViewController()
+        numberOfCards = gameSettings.currentSettings.numberOfCards
+        cardTime = gameSettings.currentSettings.cardTime
         
-        let userName: String = gameSettings.userName
+        for i in Range(uncheckedBounds: (0, numberOfCards)) {
+            indices.append(i)
+        }
+        indices = indices.shuffled()
         
-        let numberOfCards: Int = gameSettings.currentSettings.numberOfCards
-        let cardTime: Float = gameSettings.currentSettings.cardTime
-    }
-    
-    func showCardsGame() {
-        
-    }
-    
-    func userChosesCardsGame() {
-        
+        timer = Timer.scheduledTimer(timeInterval: Double(cardTime), target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
 
+    var i: Int = 0
+    @objc func fireTimer() {
+
+        if i == indices.count {
+            timer.invalidate()
+            performSegue(withIdentifier: "toChooseCardsView", sender: nil)
+        } else {
+            showCardsImageView.image = images[indices[i]]
+            i += 1
+        }
+    }
 }
